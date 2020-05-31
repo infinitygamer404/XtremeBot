ae85fa30176ec3f9b2238b76f05a603016b4670f
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2020-05-31 23:02:31 +0600

    Kwjsjdjd

--- bot.py
+++ bot.py
@@ -42,7 +42,10 @@ async def on_message(message):
         await message.channel.send("Pong! " + str(client.latency()) + "ms") # Doesn't work'
  
     if message.content.startswith("$test"):
-        await message.channel.send(isprime(str(str(message.content) + " ")[6:-1]))
+    	try:
+        	await message.channel.send(isprime(str(str(message.content) + " ")[6:-1]))
+        except IndexError:
+        	return
  
     if message.content.startswith("$info"):
         await message.channel.send("I am XtremeBot. A discord bot made by @TheXtremeCrafter#7969. (Intentional no ping)")
