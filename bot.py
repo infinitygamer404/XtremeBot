c9461b432714b0a49ac627f3fee18625aaf24a2a
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2020-05-31 12:32:55 +0600

    Changed bot prefix from "x-" to "$"

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = 'x-')
+client = commands.Bot(command_prefix = '$$')
 
 @client.event
 async def on_ready():
