37b81829172312c317549ce8e188077154df68fc
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2020-05-31 19:27:48 +0600

    Kaksjdhjd

--- bot.py
+++ bot.py
@@ -8,19 +8,25 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print("XtremeBot is ready.")
 
-@client.command()
-async def ping(ctx):
-	await ctx.send('Pong!')
+#@client.command()
+#async def ping(ctx):
+	#await ctx.send('Pong!')
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
+
     if message.content.find("$b") != -1:
         await message.channel.send("🅱️🐝")
+
     if message.content.find("$ping") != -1:
         await message.channel.send("Pong! " + str(client.latency()) + "ms")
+
     if message.content.startswith("$test"):
         await message.channel.send("test true")
-    if not message.content.startswith("$test"):
-        await message.channel.send("test false")
+
+    if message.content.startswith("$info"):
+        await message.channel.send("I am XtremeBot. A discord bot made by @TheXtremeCrafter#7969")
 
 client.run(os.getenv('Token'))
