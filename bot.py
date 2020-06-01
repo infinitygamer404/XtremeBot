8d4a44f19222e7f1461130b4890e6e728a1edbec
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2020-06-01 12:29:21 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
 import discord
 import os
 from discord.ext import commands
- 
+
 client = commands.Bot(command_prefix = '$')
 
 def isprime(num):
-	if type(num) != type(1):
-		try:
-			num = int(num)
-		except ValueError:
-			return "Incorrect input"
-	if num > 1:
-	   for i in range(2,num):
-	       if (num % i) == 0:
-	           return str(num) + " is not a prime number"
-	           return str(i) + " times " +  str(num//i) + " is " + str(num)
-	           break
-	   else:
-	       return str(num) + " is a prime number"
-	       
-	else:
-	    return str(num) + " is not a prime number"
+    if type(num) != type(1):
+        try:
+            num = int(num)
+        except ValueError:
+            return "Incorrect input"
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                return str(num) + " is not a prime number"
+                return str(i) + " times " +  str(num//i) + " is " + str(num)
+                break
+        else:
+            return str(num) + " is a prime number"
+
+    else:
+        return str(num) + " is not a prime number"
 
 @client.event
 async def on_ready():
@@ -28,27 +28,26 @@ async def on_ready():
  
 #@client.command()
 #async def ping(ctx):
-	#await ctx.send('Pong!')
+    #await ctx.send('Pong!')
  
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
- 
+
     if message.content.find("$b") != -1:
         await message.channel.send("🅱️🐝")
- 
+
     if message.content.find("$ping") != -1:
         await message.channel.send("Pong! " + str(client.latency()) + "ms") # Doesn't work'
- 
+
     if message.content.startswith("$test"):
-    	try:
-        	await message.channel.send(isprime(str(str(message.content) + " ")[6:-1]))
+        try:
+    		await message.channel.send(isprime(str(str(message.content) + " ")[6:-1]))
         except IndexError:
-        	return
- 
+            return
+
     if message.content.startswith("$info"):
         await message.channel.send("I am XtremeBot. A discord bot made by @TheXtremeCrafter#7969. (Intentional no ping)")
- 
+
 client.run(os.getenv('Token'))
- 
