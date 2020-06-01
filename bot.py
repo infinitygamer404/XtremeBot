938ad67526ff3e25fcc9aacd8997bbd58a9ed200
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2020-06-01 13:27:30 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '$')
+prefix = "$"
+
+client = commands.Bot(command_prefix = prefix)
 
 def isprime(num):
     if type(num) != type(1):
         try:
             num = int(num)
         except ValueError:
-            return "Incorrect input"
+            return "Invalid input value"
     if num > 1:
         for i in range(2,num):
             if (num % i) == 0:
@@ -35,19 +37,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.find("$b") != -1:
+    if message.content.find(prefix+"b") != -1:
         await message.channel.send("🅱️🐝")
 
-    if message.content.find("$ping") != -1:
+    if message.content.find(prefix+"ping") != -1:
         await message.channel.send("Pong! " + str(client.latency()) + "ms") # Doesn't work'
 
-    if message.content.startswith("$test"):
+    if message.content.startswith(prefix+"prime"):
         try:
             await message.channel.send(isprime(str(str(message.content) + " ")[6:-1]))
         except IndexError:
             return
+    if message.content.startswith(prefix+"eval"):
+        try:
+            await message.channel.send(eval(str(message.content)))
+        except:
+            await message.channel.send("Invalid input")
 
-    if message.content.startswith("$info"):
+    if message.content.startswith(prefix+"help"):
+        await message.channel.send("Commands list:\n`b`\n`prime`\n`eval`\n`info`")
+
+    if message.content.startswith(prefix+"info"):
         await message.channel.send("I am XtremeBot. A discord bot made by @TheXtremeCrafter#7969. (Intentional no ping)")
 
 client.run(os.getenv('Token'))
