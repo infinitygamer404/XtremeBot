e436600c414b0562dce3e5fb45161c740f12722b
infinitygamer404 <59830101+infinitygamer404@users.noreply.github.com>
2020-08-08 14:48:45 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,37 +7,45 @@ PREFIX = "./"
 
 client = commands.Bot(command_prefix = PREFIX)
 
+# Ready stuff
 @client.event
 async def on_ready():
     print("XtremeBot is ready.")
     await mf.log()
 
+# Errors
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Didn't find a command with that name, type `./help` for a list of commands. \u274c")
 
+# Ping
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! ` {round(client.latency * 1000)} `ms')
 
+#B
 @client.command()
 async def b(ctx):
     await ctx.send("🅱️🐝")
 
+# Primechecker
 @client.command(aliases=["isprime"])
 async def prime(ctx, num):
     await ctx.send(str(mf.isprime(num)))
 
+# Fibonacci
 @client.command()
 async def fib(ctx, num):
     await ctx.send(str(mf.fib(num)))
 
+# Say
 @client.command()
 async def say(ctx, *, msg):
     await ctx.message.delete()
     await ctx.send(msg)
 
+# Bad evaluates
 @client.command(aliases=["eval"])
 async def _eval(ctx, *, stmt):
     try:
@@ -45,9 +53,9 @@ async def _eval(ctx, *, stmt):
     except:
         await ctx.send("Invalid input")
 
-@client.command(aliases=["help"])
-async def _help(ctx):
-    await ctx.send(f"The bot prefix is `{PREFIX}`\nCommands list:\n`{prefix}b` : Just B.\n`{prefix}prime` : Check if a number is prime or not.\n`{prefix}eval` : Evaluate your [mathematical] statement. This accepts Python syntax.\n`{prefix}say` : Says whatever you say after the command.\n`{prefix}ping` : Pong!\n`{prefix}info` : Information about the bot.")
+##@client.command(aliases=["help"])
+##async def _help(ctx):
+##    await ctx.send(f"The bot prefix is `{PREFIX}`\nCommands list:\n`{prefix}b` : Just B.\n`{prefix}prime` : Check if a number is prime or not.\n`{prefix}eval` : Evaluate your [mathematical] statement. This accepts Python syntax.\n`{prefix}say` : Says whatever you say after the command.\n`{prefix}ping` : Pong!\n`{prefix}info` : Information about the bot.")
 
 @client.event
 async def on_message(message):
